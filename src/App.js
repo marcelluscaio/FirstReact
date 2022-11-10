7c77493855fba92b3fbdda5dc9f88a52fbeba269
Caio Cabral <ocaiocabral@gmail.com>
2022-11-10 11:58:29 -0400

    Input field component creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Banner from './componentes/banner/banner'
+import Banner from './componentes/banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Banner />
+        <CampoTexto />
       </header>
     </div>
   );
