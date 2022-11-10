0033e1a8b239dbe8426d1e65c1b4ff1fda5302c7
Caio Cabral <ocaiocabral@gmail.com>
2022-11-10 11:22:35 -0400

    Project restructuring and field creation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './componentes/banner/banner'
 
 function App() {
@@ -7,18 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Banner />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Caio's first React project
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
