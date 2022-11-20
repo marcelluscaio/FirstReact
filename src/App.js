3a3c68f8b3eff8d46f07b548bb0a49b5d91d3dc8
Caio Cabral <ocaiocabral@gmail.com>
2022-11-20 11:00:25 -0400

    Refactoring to pass stages using stages.map

--- src/App.js
+++ src/App.js
@@ -6,15 +6,39 @@ import {useState} from 'react';
 function App() {
   const [colaboradores, setColaboradores] = useState([]);
 
+  const palcos = [
+    {
+      nome: 'Metal',
+      cor1: '#57C278',
+      cor2: '#D9F7E9'
+    },
+    {
+      nome: 'Punk',
+      cor1: '#82CFFA',
+      cor2: '#E8F8FF'
+    },
+    {
+      nome: 'Grunge',
+      cor1: '#A6D157',
+      cor2: 'F0F8E2'
+    },
+    {
+      nome: 'Desistimos de classificar',
+      cor1: '#E06B69',
+      cor2: '#FDE7E8'
+    },
+  ];
+
   return (
     <div className="App">
       <Banner />
       <Formulario cadastrarColaborador= {colaborador => {
         setColaboradores([...colaboradores, colaborador]);
       }} />
-      <Palco nome={"Metal"} />
+      {palcos.map(palco => <Palco key={palco.nome} nome={palco.nome}/>)}
+      {/* <Palco nome={"Metal"} />
       <Palco nome={"Punk"} />
-      <Palco nome={"Grunge"} />
+      <Palco nome={"Grunge"} /> */}
 
     </div>
   );
