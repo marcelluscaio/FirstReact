ce36a29affe6912ef983b0de9e5a446353cce126
Caio Cabral <ocaiocabral@gmail.com>
2022-11-21 16:18:44 -0400

    Creating cards

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './colaborador.css';
+
+const Colaborador = () => {
+   return(
+      <div className='colaborador'>
+         <div className='cabecalho'>
+            <img src='https://github.com/marcelluscaio.png' alt='foto do Caio'/>
+         </div>
+         <div className='rodape'>
+            <h4>Caio Cabral</h4>
+            <h5>Instrutor</h5>
+         </div>
+      </div>
+   
+   );
+};
+
+export default Colaborador;
--- src/componentes/Palcos/index.js
+++ src/componentes/Palcos/index.js
@@ -1,9 +1,12 @@
 import './palcos.css';
+import Colaborador from '../Colaborador';
 
 const Palco = (props) => {
    return(
       <section className='palco' style={{backgroundColor: props.cor2}}>
          <h3 className='palco__titulo' style={{borderColor: props.cor1}}>{props.nome}</h3>
+         <Colaborador />
+
       </section>
    )
 }
