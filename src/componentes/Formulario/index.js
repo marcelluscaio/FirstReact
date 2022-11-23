b9c72697a4b7b52459f4ff91ea17c97346251076
Caio Cabral <ocaiocabral@gmail.com>
2022-11-23 12:41:30 -0400

    Finishing and setting next steps

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,7 +5,6 @@ import './formulario.css';
 import {useState} from 'react';
 
 const Formulario = (props) => {
-   
 
    const [nome, setNome] = useState('');
    const [cargo, setCargo] = useState('');
