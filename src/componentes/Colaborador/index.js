81943717bcfc58ebb86a6de31da0abf966bb14b0
Caio Cabral <ocaiocabral@gmail.com>
2022-11-23 10:38:37 -0400

    Finished project

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './colaborador.css';
 
-const Colaborador = ({nome, imagem, cargo}) => {
+const Colaborador = ({nome, imagem, cargo, cor}) => {
    return(
       <div className='colaborador'>
-         <div className='cabecalho'>
+         <div className='cabecalho' style={{backgroundColor: cor}}>
             <img src={imagem} alt={nome}/>
          </div>
          <div className='rodape'>
-            <h4>{nome}</h4>
-            <h5>{cargo}</h5>
+            <h4 style={{color: cor}}>{nome}</h4>
+            <h5 style={{color: cor}}>{cargo}</h5>
          </div>
       </div>
    
